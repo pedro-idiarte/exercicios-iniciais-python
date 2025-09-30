dd9f60ba5fb4d833dfa0b730a0b735f2b6d0bfcf
Pedro Idiarte Alves <pedroidiarte7@gmail.com>
2025-09-30 18:10:26 -0300

    desafio em aula combustivel resolvido

---
+++ desafio_aula_combustivel/desafio_aula_combustivel.py
@@ -0,0 +1,22 @@
+GASOLINA = 4.97
+ALCOOL = 5.78
+valor = 0.0
+
+nome = input("Digite seu nome: ")
+
+print(f"Olá {nome}!!!")
+
+combustivel = input("Digite G - Gasolina e A - Alcool: ")
+letra = combustivel.upper()
+quantidade = float(input("Digite a quantidade de litros: "))
+
+if letra == "G" and quantidade > 0:
+    valor = quantidade * GASOLINA
+   
+if letra == "A" and quantidade > 0:
+    valor = quantidade * ALCOOL
+
+if letra == "G" or letra == "A":
+    print(f"Valor total: {valor:.2f}.")
+else:
+    print("Valores invalidos.")
