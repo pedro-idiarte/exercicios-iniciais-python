66a5e6f03bdb5038fcf34e36bea6330be8b46a48
Pedro Idiarte Alves <pedroidiarte7@gmail.com>
2025-10-01 16:51:11 -0300

    adicionado exemplos e pastas

---
+++ exemplo_condicionais/time_coração.py
@@ -0,0 +1,12 @@
+time_coracao = input("Digite o time do coração: ")
+
+time_coracao = time_coracao.upper
+
+if time_coracao == "GREMIO" or time_coracao == "GREMIO":
+    print(f"Voce é Gremista")
+elif time_coracao == "INTER":
+    print(f"Voce precisa escolher um time bom!")
+elif time_coracao == "JUVENTUDE":
+    print("Voce está no caminho!")
+else:
+    print(f"Voce nao digitou um time avaliado!")
